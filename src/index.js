83aaeb6eb39d61d7a7d35bca2d4f3673fc9fa52b
Gaurav suryavanshi <80798531+gauravsuryvanshi06@users.noreply.github.com>
2024-07-07 19:03:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './webReport';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+
+
+reportWebVitals();
